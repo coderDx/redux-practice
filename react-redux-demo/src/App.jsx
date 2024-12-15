f9036bac64fde978e0629bda8de7e1207988d495
CoderDx <info@kerbob.io>
2024-12-15 13:32:20 -0600

    Video 26

--- react-redux-demo/src/App.jsx
+++ react-redux-demo/src/App.jsx
@@ -6,10 +6,13 @@ import CakeContainer from './Components/CakeContainer'
 import IceCreamContainer from './Components/IceCreamContainer'
 import HooksCakeContainer from './Components/HooksCakeContainer'
 import NewCakeContainer from './Components/NewCakeContainer'
+import ItemContainer from './Components/ItemContainer'
 
 function App() {
   return (
     <Provider store={store}>
+      <ItemContainer cake />
+      <ItemContainer />
       <HooksCakeContainer />
       <CakeContainer />
       <IceCreamContainer />
